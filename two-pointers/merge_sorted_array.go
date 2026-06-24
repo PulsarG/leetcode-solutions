8e4020ee58397e06c39a8105bffe652cc45aad9d
PulsarG <pulsar.g.projects@gmail.com>
2026-06-24 11:08:24 +0300

    feat: 2poiters merge_sorted_array

---
+++ two-pointers/merge_sorted_array.go
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/merge-sorted-array/description/?envType=problem-list-v2&envId=two-pointers
+// easy
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	nums1 := []int{0}
+	nums2 := []int{1}
+
+	m := 0
+	//n := 3
+
+	nums1 = append(nums1[:m], nums2...)
+	sort.Ints(nums1)
+	fmt.Println(nums1)
+}
