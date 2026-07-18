3024abde957feb6521504f9e92dcb36f014cdbb6
PulsarG <pulsar.g.projects@gmail.com>
2026-07-19 01:16:39 +0300

    feat: 48

---
+++ matrix/rotate_matrix.go
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/rotate-image/description/?envType=problem-list-v2&envId=matrix
+// 48
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Run())
+}
+
+func Run() [][]int {
+	m := [][]int{{5, 1, 9, 11},
+		{2, 4, 8, 10},
+		{13, 3, 6, 7},
+		{15, 14, 12, 16}}
+
+	i, j, l := 0, 0, len(m[0])
+	for i < l {
+		for j < (l - i - 1) {
+			tmp := m[i][j]
+			x, y := i, j
+			for k := 0; k < 4; k++ {
+				z := l - x - 1
+				w := m[y][z]
+				m[y][z] = tmp
+				tmp = w
+				x, y = y, z
+			}
+			j++
+		}
+		i++
+		j = i
+	}
+
+	return m
+}
